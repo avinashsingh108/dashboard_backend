b9f0869aa6310156c22c1c05d50ea9cf8751f966
avinashsingh108 <TsaPh@69@HMS108>
2025-02-03 23:38:35 +0530

    add frontend link

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require('./routes/routes');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://dashboard-frontend-flame-nu.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
